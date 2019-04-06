815b3c21cde0b3480b772d3f54b65b9faab96658
Azhar Shah <azharshahstl@yahoo.com>
2019-04-06 19:09:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get '/multiply/:num1/:num2' do 
+  get '/multiply/:num1/:num2' do
+    binding.pry
     @product = params[:num1].to_i * params[:num2].to_i
     @product
   end
